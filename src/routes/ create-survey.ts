0b543f6ae8d7bf973f30aa9667d844c028de75f7
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2024-02-07 13:48:38 -0300

    add Prisma query logging and create options for surveys

--- src/routes/ create-survey.ts
+++ src/routes/ create-survey.ts
@@ -6,17 +6,24 @@ export async function createSurvey(app: FastifyInstance) {
     app.post('/surveys', async (request, reply) => {
         const createSurveySchema = z.object({
             title: z.string(),
+            options: z.array(z.string())
         }) 
 
-        const { title } = createSurveySchema.parse(request.body);
+        const { title, options } = createSurveySchema.parse(request.body);
 
         const survey = await prisma.survey.create({
             data: {
-                title
+                title,
+                options: {
+                    createMany: {
+                        data: options.map((option) => {
+                            return { title: option }
+                        })
+                    }
+                }
             }
         })
 
         return reply.status(201).send({surveyId: survey.id});
     })
-    
 }
