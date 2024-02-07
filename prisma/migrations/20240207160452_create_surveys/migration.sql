8b7fa716a1dac08625d2d89b29b2a22c632305f6
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2024-02-07 13:06:29 -0300

    add surveys table and schema

---
+++ prisma/migrations/20240207160452_create_surveys/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "surveys" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "surveys_pkey" PRIMARY KEY ("id")
+);
